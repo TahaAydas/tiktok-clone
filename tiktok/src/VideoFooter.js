8bd6fc4f75ffd92a9d5e8fb0fa0bf55e8c112268
Taha <you@example.com>
2021-01-16 00:56:31 +0300

    Adding video player components

---
+++ tiktok/src/VideoFooter.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./VideoFooter.css";
+function VideoFooter() {
+  return (
+    <div className="videoFooter">
+      <div className="videoFooter__text">
+        <h3>@10numaramizah</h3>
+        <p>Here is some description</p>
+      </div>
+      ;
+    </div>
+  );
+}
+
+export default VideoFooter;
