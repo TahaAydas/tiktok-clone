2b6cf43c7c266cf05d0f8a47d4cd18c974fd3fc5
Taha <you@example.com>
2021-01-16 03:54:58 +0300

    Adding Video Sidebar Buttons

--- tiktok/src/Video.js
+++ tiktok/src/Video.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useRef, useState } from "react";
 import "./Video.css";
 import VideoFooter from "./VideoFooter";
+import VideoSidebar from "./VideoSidebar";
 
 function Video() {
   const [playing, setPlaying] = useState(false);
@@ -29,7 +30,7 @@ function Video() {
         src="https://v77.tiktokcdn.com/a1fc4441651280a7a17f15e96d1237c6/60024906/video/tos/alisg/tos-alisg-pve-0037c001/0565ff41c81d4e6795b9eeb79af6450e/?a=1233&br=2998&bt=1499&cd=0%7C0%7C1&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=202101152001230101902192114921412C&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=amY7cTk3ZnE6eTMzNjczM0ApNDs1Njs5ZDs4NzM2ZmYzOGdrLW9tNC5wX2ZfLS1eMTRzczBeMjE1MmJeMmA2M2EyLjA6Yw%3D%3D&vl=&vr="
       ></video>
       <VideoFooter />
-      {/* Videosidebar */}
+      <VideoSidebar />
     </div>
   );
 }
--- tiktok/src/VideoSidebar.js
+++ tiktok/src/VideoSidebar.js
@@ -1,8 +1,28 @@
 import React from "react";
 import "./VideoSidebar.css";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import MessageIcon from "@material-ui/icons/Message";
+import ShareIcon from "@material-ui/icons/Share";
 
 function VideoSidebar() {
-  return <div className="videoSidebar"></div>;
+  return (
+    <div className="videoSidebar">
+      <div className="videoSidebar__button">
+        <FavoriteIcon fontSize="large" />
+        <p>100</p>
+      </div>
+
+      <div className="videoSidebar__button">
+        <MessageIcon fontSize="large" />
+        <p>100</p>
+      </div>
+
+      <div className="videoSidebar__button">
+        <ShareIcon fontSize="large" />
+        <p>100</p>
+      </div>
+    </div>
+  );
 }
 
 export default VideoSidebar;
